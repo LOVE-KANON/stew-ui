2c8b6eb2909b5ba0822daa731b80c5f9b200238a
HikaruMaruyama <Hikaru.Maruyama@example.com>
2025-12-28 21:17:11 +0900

    AppProviderにQueryClientの実装

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { queryConfig } from '@/config/react-query';
 
 type AppProviderProps = {
     children: React.ReactNode;
 };
 
 export const AppProvider = ({ children }: AppProviderProps) => {
+    const [queryClient] = React.useState(
+        () => new QueryClient({
+            defaultOptions: queryConfig,
+        }),
+    );
     return (
-        <>{ children }</>
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
     );
 }
