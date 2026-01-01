a8ca4d3e906fc529feb817d88de3952aead74114
HikaruMaruyama <Hikaru.Maruyama@example.com>
2026-01-02 03:24:47 +0900

    フォルダ構成のリファクタリング(features/core直下に「shell」を移動、shellはAPの基盤部分の処理を包括)

--- src/app/root.tsx
+++ src/app/root.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 import { AppBar } from "@mui/material";
-import { Header } from "@/features/core/components/shell/Header";
+import { Header } from "@/features/core/shell/components";
 
 export const ErrorBoundary = () => {
     return <div>Something went wrong!</div>;
